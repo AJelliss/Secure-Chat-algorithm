13d6a4959c5273ccbc66e980eef3cdc3527f863d
Andrew <andrew.jelliss@slu.edu>
2024-04-17 17:00:34 -0500

    fixed server big. created diffieHellman file.

--- server.cpp
+++ server.cpp
@@ -8,6 +8,7 @@
 #include <mutex>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <algorithm>
 
 const int PORT = 8080;
 const int MAX_CONNECTIONS = 10;
@@ -24,7 +25,6 @@ void handleClient(int clientSocket) {
             break;
         }
         std::cout << "Received: " << buffer << std::endl;
-        // Broadcast received message to all clients
         mtx.lock();
         for (int socket : clientSockets) {
             if (socket != clientSocket) {
@@ -34,7 +34,6 @@ void handleClient(int clientSocket) {
         mtx.unlock();
         memset(buffer, 0, 1024);
     }
-    // Close client socket and remove from the list
     close(clientSocket);
     mtx.lock();
     clientSockets.erase(std::remove(clientSockets.begin(), clientSockets.end(), clientSocket), clientSockets.end());
@@ -47,13 +46,11 @@ int main() {
     int opt = 1;
     int addrlen = sizeof(address);
 
-    // Create socket
     if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
     if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
@@ -62,13 +59,11 @@ int main() {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Bind socket to port 8080
     if (bind(serverSocket, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // Start listening
     if (listen(serverSocket, MAX_CONNECTIONS) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
@@ -76,17 +71,14 @@ int main() {
 
     std::cout << "Server listening on port " << PORT << std::endl;
 
-    // Accept incoming connections
     while (true) {
         if ((clientSocket = accept(serverSocket, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
-        // Add client socket to the list
         mtx.lock();
         clientSockets.push_back(clientSocket);
         mtx.unlock();
-        // Create a new thread to handle the client
         std::thread(handleClient, clientSocket).detach();
     }
 
